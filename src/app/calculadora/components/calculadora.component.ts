b84232261bf7fa9931476acea2650325786871cb
MadalenaCampos <madacampos02@gmail.com>
2022-04-13 15:38:10 -0300

    projeto concluido

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -7,10 +7,91 @@ import { CalculadoraService } from '../services';
   styleUrls: ['./calculadora.component.less'],
 })
 export class CalculadoraComponent implements OnInit {
+  // Atributos privados => não podem ser acessados fora dessa classe.
+  private numero1!: string;
+  private numero2!: string;
+  private resultado!: number;
+  private operacao!: string;
+
   constructor(private calculadoraService: CalculadoraService) {}
   // Significa que o typescript criará automaticamente um atributo na classe, chamada calculadoraService.
   // Através do this.calculadoraService, eu consigo acessar os métodos do serviço.
 
   // Ciclo de vida do angular, ao inicializar. Usado, principalmente, para consultas externas.
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.limpar();
+  }
+
+  limpar(): void {
+    this.numero1 = '0';
+    this.numero2 = null;
+    this.resultado = null;
+    this.operacao = null;
+  } // Irá zerar os valores dos atributos.
+
+  adicionarNumero(numero: string): void {
+    if (this.operacao === null) {
+      this.numero1 = this.concatenarNumero(this.numero1, numero);
+    } else {
+      this.numero2 = this.concatenarNumero(this.numero2, numero);
+    }
+  } // Irá adicionar o número ao atributo numero1 ou numero2.
+
+  concatenarNumero(numAtual: string, numConcat: string): string {
+    if (numAtual === '0' || numAtual === null) {
+      numAtual = '';
+    } // Se o número atual for 0 ou null, o número atual será vazio.
+
+    if (numConcat === '.' && numAtual === '') {
+      numAtual = '0.';
+    } // Se o número concatenado for um ponto e o número atual for vazio, o primeiro número será 0 ==> ex.: 0.1
+
+    if (numConcat === '.' && numAtual.indexOf('.') > -1) {
+      return numAtual;
+    } // Se o número concatenado for um ponto e o número atual já tiver um ponto, o número concatenado não será adicionado. Pois não pode ter dois pontos.
+
+    return numAtual + numConcat;
+  }
+
+  definirOperacao(operacao: string): void {
+    if (this.operacao === null) {
+      this.operacao = operacao;
+      return;
+    } // Se a operação for nula, a operação será definida.
+
+    if (this.numero2 !== null) {
+      this.resultado = this.calculadoraService.calcular(
+        parseFloat(this.numero1),
+        parseFloat(this.numero2),
+        this.operacao
+      );
+      this.operacao = operacao;
+      this.numero1 = this.resultado.toString();
+      this.numero2 = null;
+      this.resultado = null;
+    } // Se ele possui os 2 números e a operação, o resultado será calculado.
+  }
+
+  calcular(): void {
+    if (this.numero2 === null) {
+      return;
+    } // Se não tiver o segundo número, não faz nada se apertar o igual.
+
+    this.resultado = this.calculadoraService.calcular(
+      parseFloat(this.numero1),
+      parseFloat(this.numero2),
+      this.operacao
+    ); // Calcula o resultado.
+  }
+
+  get display(): string {
+    // get display => convenção do typescript, que gera um atributo de classe chamado display, e cria um getter para acessar esse atributo.
+    if (this.resultado !== null) {
+      return this.resultado.toString();
+    }
+    if (this.numero2 !== null) {
+      return this.numero2;
+    }
+    return this.numero1;
+  } // Retorna o número que será exibido na tela.
 }
--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -5,7 +5,50 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CalculadoraService {
+  // Criação de constantes para representar as operações, já que os mesmos são usados diversas vezes.
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+  // static => facilita o acesso ao atributo, permitindo chamar a classe, sem uma instancia dela.
+  // readonly => somente leitura.
+
   constructor() {}
+
+  public calcular(num1: number, num2: number, operacao: string): number {
+    // :number => retorno da função.
+
+    let resultado: number;
+    // Armazena o resultado da operação.
+    // let => variável local.
+
+    switch (operacao) {
+      // switch => faz a comparação com o valor da variável.
+
+      case CalculadoraService.SOMA:
+        // case => caso a condição seja verdadeira, executa o código.
+
+        resultado = num1 + num2;
+        break; // break => finaliza o switch.
+
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+        break;
+
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+
+      default:
+        resultado = 0;
+      // Caso nenhuma das condições seja verdadeira, o retorno será 0.
+    }
+
+    return resultado;
+  }
 }
 
 // Regras de negócios e servidores externos(API).
